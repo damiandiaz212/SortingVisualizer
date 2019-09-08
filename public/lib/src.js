5be7e1490633c01435d69cabe0ab662d7c1893f3
Damian Diaz <damiandiaz2025@gmail.com>
2019-09-08 19:22:07 -0400

    began comparison implemenation

--- public/lib/src.js
+++ public/lib/src.js
@@ -1,106 +1,117 @@
-var canvas=document.getElementById("canvas");
-var ctx=canvas.getContext("2d");
-var cw=canvas.width;
-var ch=canvas.height;
+var canvas1 = document.getElementById("canvas1");
+var canvas2 = document.getElementById("canvas2");
 
-var rows=9;
-var cols=9;
+var ctx1 = canvas1.getContext("2d");
+var ctx2 = canvas2.getContext("2d");
 
-var img=new Image();
-img.onload=start;
-img.src="images/test.jpg";
+var rows = 15;
+var cols = 15;
 
-let pieces = []
+var img = new Image();
+img.onload = start;
+img.src = "images/test.jpg";
+
+let pieces1 = [];
+let pieces2 = [];
 let speed = 250;
 
-function start(){
+function start() {
+  pieces1 = CreateBoard(rows, cols);
+  pieces1 = Shuffle(pieces1);
 
-    pieces = CreateBoard(rows, cols);
-    pieces = Shuffle(pieces);
-    RefreshBoard();
+  pieces2 = CreateBoard(rows, cols);
+  pieces2 = Shuffle(pieces2);
 
+  RefreshBoard(canvas1, ctx1, pieces1);
+  RefreshBoard(canvas2, ctx2, pieces2);
 }
 
-function Reset(){
-    pieces = Shuffle(pieces);
-    RefreshBoard();
+function Reset() {
+  pieces1 = Shuffle(pieces);
+  RefreshBoard(ctx1, pieces1);
 }
 
-function ChangeSpeed(val){
-    speed = val;
+function ChangeSpeed(val) {
+  speed = val;
 }
 
-
-function BubbleSort(){
-
-    for(var i = 0; i < pieces.length; i++) {
-        (function(i){
-            window.setTimeout(function(){
-            for(var j = 1; j < pieces.length; j++) {
-                if(pieces[j - 1].val > pieces[j].val) {
-                        swap(pieces, j - 1, j);
-                        RefreshBoard();
-                    }
-                }
-            }, i * speed);
-        }(i));
-    }
-
-
+function Sort() {
+  BubbleSort(pieces1);
 }
 
-function RefreshBoard(){
-
-    let iw=canvas.width=img.width;
-    let ih=canvas.height=img.height;
-    let pieceWidth=iw/cols;
-    let pieceHeight=ih/rows;
-
-    let i=0;
-    for(let y=0;y<rows;y++){
-        for(let x=0;x<cols;x++){
-            let p = pieces[i++];
-            ctx.drawImage(img, x*pieceWidth, y*pieceHeight, pieceWidth, pieceHeight, p.col*pieceWidth, p.row*pieceHeight, pieceWidth, pieceHeight);
-        }           
-    }
-}
-
-function CreateBoard(row, col){
-
-    let pieces = [];
-    let n = 0;
-
-    for(let i=0; i<rows;i++){
-        for(let j=0; j<cols; j++){
-            pieces.push(new Piece(n++, i, j));
+function BubbleSort(array) {
+  for (var i = 0; i < array.length; i++) {
+    (function(i) {
+      window.setTimeout(function() {
+        for (var j = 1; j < array.length; j++) {
+          if (array[j - 1].val > array[j].val) {
+            swap(array, j - 1, j);
+            RefreshBoard(ctx1, array);
+          }
         }
-    }
+      }, i * speed);
+    })(i);
+  }
+}
 
-    return pieces;
+function RefreshBoard(canvas, ctx, pieces) {
+  let iw = (canvas.width = img.width);
+  let ih = (canvas.height = img.height);
+  let pieceWidth = iw / cols;
+  let pieceHeight = ih / rows;
+
+  let i = 0;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      let p = pieces[i++];
+      ctx.drawImage(
+        img,
+        x * pieceWidth,
+        y * pieceHeight,
+        pieceWidth,
+        pieceHeight,
+        p.col * pieceWidth,
+        p.row * pieceHeight,
+        pieceWidth,
+        pieceHeight
+      );
+    }
+  }
+}
+
+function CreateBoard(row, col) {
+  let pieces = [];
+  let n = 0;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      pieces.push(new Piece(n++, i, j));
+    }
+  }
+
+  return pieces;
 }
 
 function swap(array, i, j) {
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
 
-function Piece(val, row, col){
-    this.val = val;
-    this.col=col; 
-    this.row=row;
-};
+function Piece(val, row, col) {
+  this.val = val;
+  this.col = col;
+  this.row = row;
+}
 
-function Shuffle(a){
-  for(let j, x, i = a.length; i; j = Math.floor(Math.random() * i), x = a[--i], a[i] = a[j], a[j] = x);
+function Shuffle(a) {
+  for (
+    let j, x, i = a.length;
+    i;
+    j = Math.floor(Math.random() * i), x = a[--i], a[i] = a[j], a[j] = x
+  );
   return a;
-};
+}
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
+// todo
+function CanvasObject(
